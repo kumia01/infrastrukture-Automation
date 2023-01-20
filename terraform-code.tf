b71224848e4141cc4fb09f638561352658ab3a40
kumia01 <71126246+kumia01@users.noreply.github.com>
2023-01-20 14:48:00 +0100

    updating terraform code

--- terraform-code.tf
+++ terraform-code.tf
@@ -1,8 +1,9 @@
 provider "aws" {
   region = "us-west-2"
-  access_key = var.access_key
-  secret_key = var.secret_key
+  access_key = var.AWS_ACCESS_KEY_ID
+  secret_key = var.AWS_SECRET_ACCESS_KEY
 }
+
 
 resource "aws_vpc" "example" {
   cidr_block = "10.0.0.0/16"
