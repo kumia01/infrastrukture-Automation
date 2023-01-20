70332ba1a02115cc39583851998da8c642f897ce
kumia01 <71126246+kumia01@users.noreply.github.com>
2023-01-20 15:14:59 +0100

    update terraform

--- terraform-code.tf
+++ terraform-code.tf
@@ -1,5 +1,13 @@
+terraform {
+  required_providers {
+    aws = {
+      source = "hashicorp/aws"
+      version = "4.51.0"
+    }
+  }
+}
+
 provider "aws" {
-   version = "4.51.0"
   region = "us-west-2"
   access_key = var.AWS_ACCESS_KEY_ID
   secret_key = var.AWS_SECRET_ACCESS_KEY
